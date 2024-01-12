d5bac73eaa9283ab902e6efa5da465b18879587e
Belupe <108235745+Belupe@users.noreply.github.com>
2024-01-12 01:02:40 +0100

    Add files via upload

---
+++ src/utils/common.ts
@@ -0,0 +1,22 @@
+export interface Auth {
+  type: 'web-api' | 'official' | 'azure' | 'palm' | 'gemini';
+  [propName: string]: any;
+}
+
+export function checkAuth (auth: Auth): boolean {
+  return (
+    auth &&
+    ((auth.type === 'web-api' && !!auth.accessToken) ||
+      (auth.type === 'official' && !!auth.apiKey) ||
+      (auth.type === 'azure' && !!auth.azureAPIKey) ||
+      (auth.type === 'palm' && !!auth.palmAPIKey) ||
+      (auth.type === 'gemini' && !!auth.geminiAPIKey))
+  )
+}
+
+export function forceNumber (val: any) {
+  if (val === '') {
+    return 0
+  }
+  return isNaN(Number(val)) ? 0 : Number(val)
+}
